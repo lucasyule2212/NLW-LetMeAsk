8a0b07841050209b2596fd8a9137d2a7c84fc5a1
Lucas Yule <lyrma@cin.ufpe.br>
2021-06-20 23:43:30 -0300

    Day 1 commit

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from './components/Button';
+
+
+function App() {
+  return (
+    <div>
+    <h1>Hello World!</h1>
+    <Button></Button>
+    </div>
+  );
+}
+
+export default App;
